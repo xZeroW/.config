d24c7cd69a05b1a8c9318f876c79b838b78dc9f6
xZeroW <vic_marts@hotmail.com>
2021-12-04 20:55:05 -0300

    install script test

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!#!/bin/bash
+ARCH_PACKAGES="git yay polybar rofi zsh brave-bin alacritty gotop-bin bat neovim bspwm sxhkd ttf-fira-code dunst discord-canary noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-emoji font-manager-git picom-ibhagwan-git"
+
+echo " > Install arch packages"
+pacman -S --needed --noconfirm $USER_ARCH_PACKAGES
+
+echo " > Set zsh as default shell for user"
+sudo -u vu chsh -s /usr/bin/zsh
+
+echo " > Symlink configs files"
+ln -s .dotfiles $HOME/.config
+
+echo "Install Done!"
