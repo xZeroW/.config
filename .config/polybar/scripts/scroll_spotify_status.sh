b2d1e52d535787a111a5066252b2cb607dc47b89
xZeroW <vic_marts@hotmail.com>
2021-12-05 04:24:25 -0300

    fix spotify japanese glyphs

--- .config/polybar/scripts/scroll_spotify_status.sh
+++ .config/polybar/scripts/scroll_spotify_status.sh
@@ -2,7 +2,7 @@
 
 # see man zscroll for documentation of the following parameters
 zscroll -l 20 \
-  --delay 0.1 \
+  --delay 0.5 \
   --scroll-padding "  " \
   --match-command "$(dirname $0)/get_spotify_status.sh --status" \
   --match-text "Playing" "--scroll 1" \
