01c61cc457bb83b18b9e9f5b0d1593d1061f0094
xZeroW <vic_marts@hotmail.com>
2021-12-04 21:59:22 -0300

    fix root permission

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 ARCH_PACKAGES="git yay polybar rofi zsh brave-bin alacritty gotop-bin bat neovim bspwm sxhkd ttf-fira-code dunst discord-canary noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-emoji font-manager-git picom-ibhagwan-git btop"
 
 echo " > Install arch packages"
-pacman -S --needed --noconfirm $(echo $ARCH_PACKAGES)
+sudo pacman -S --needed --noconfirm $(echo $ARCH_PACKAGES)
 
 echo " > Set zsh as default shell for user"
 sudo -u $USER chsh -s /usr/bin/zsh
