61c57c91f8c0b5848fe143a1f2810a008ea56d72
xZeroW <vic_marts@hotmail.com>
2021-12-07 02:00:54 -0300

    more packages

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ARCH_PACKAGES="git yay polybar rofi zsh brave-bin alacritty gotop-bin bat neovim bspwm sxhkd ttf-fira-code dunst discord-canary noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-emoji font-manager-git picom-ibhagwan-git btop"
+ARCH_PACKAGES="git yay polybar rofi zsh feh bitwarden brave-bin alacritty gotop-bin bat neovim bspwm sxhkd ttf-fira-code dunst discord-canary noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-emoji font-manager-git picom-ibhagwan-git btop spotifyd-full-git spotify-tui"
 
 echo " > Install arch packages"
 sudo pacman -S --needed --noconfirm $(echo $ARCH_PACKAGES)
